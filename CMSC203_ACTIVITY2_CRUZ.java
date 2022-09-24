54bc80109e86e1ed6b9303c7e6ea9660744fc644
RavenCha <113488948+RavenCha@users.noreply.github.com>
2022-09-25 01:44:42 +0800

    Add files via upload

---
+++ CMSC203_ACTIVITY2_CRUZ.java
@@ -0,0 +1,20 @@
+package CMSC203_ACTIVITY2_CRUZ;
+public class CMSC203_ACTIVITY2_CRUZ {
+    public static void main(String [] args) {
+   	 
+   	String name = "Chandro Cruz" ;
+   	int age = 19;
+   	float gpa=  1.74f;
+       String bloodtype = " A ";
+       
+       
+       System.out.println("My name is " + name );
+       System.out.println("I am " + age + " years old ");
+       System.out.println("My GPA is " + gpa );
+       System.out.println("My blood type is" + bloodtype);
+       		
+      
+   	    	 
+   	 
+   	}
+}
