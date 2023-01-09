3189ab977ecad50444b673d983e484a65b99e9ae
RavenCha <113488948+RavenCha@users.noreply.github.com>
2023-01-09 21:36:35 +0800

    Add files via upload

---
+++ Activity6/src/ClassesandObject.java
@@ -0,0 +1,17 @@
+public class ClassesandObject {
+   public static void main (String[]args ) 
+   {
+	   
+	   Cat p = new Cat ();
+	   p.name = "Emori";
+	   p.breed = "Persian Cat";
+	   p.age = 2 ;
+	   p.color = "White";
+	   
+	   System.out.println(p.name);
+	   System.out.println(p.breed);
+	   System.out.println (p.age);
+	   System.out.println(p.color);
+	   
+   }
+}
